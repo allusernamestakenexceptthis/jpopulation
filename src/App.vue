ba70c47076a586add2640bddde5ee98ac26d02d0
athary <athary80@gmail.com>
2023-10-15 12:47:26 +0900

    chore: #1 vite vue3 最初構築

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import ChartView from '@/views/ChartView.vue'
+</script>
+
+<template>
+  <ChartView />
+</template>
---
+++ src/views/ChartView.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+// main chart view
+</script>
+<template>
+  <div>Start of a good thing</div>
+</template>
