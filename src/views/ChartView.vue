b061199fe46627c9684180637d83bf190e2dc18b
athary <athary80@gmail.com>
2023-10-30 05:16:02 +0900

    fix: チェックボックスとラジオのテクストをクリックしても影響ない

--- src/views/ChartView.vue
+++ src/views/ChartView.vue
@@ -75,10 +75,15 @@ usePopulationStore()
         <input
           v-model="selectedPrefectures[prefecture.prefCode]"
           type="checkbox"
-          class="w-4 h-4 border-2 rounded-sm shrink-0 mt-1 mx-2 accent-blue-800 checked:border-0"
+          class="w-4 h-4 border-2 rounded-sm shrink-0 mt-1 mx-2 accent-blue-800 checked:border-0 cursor-pointer"
           :name="'selectedPrefectures_' + prefecture.prefCode"
         />
-        <label :for="'selectedPrefectures_' + prefecture.prefCode" class="whitespace-nowrap">{{ prefecture.prefName }}</label>
+        <label
+          :for="'selectedPrefectures_' + prefecture.prefCode"
+          class="whitespace-nowrap cursor-pointer"
+          @click="selectedPrefectures[prefecture.prefCode] = !selectedPrefectures[prefecture.prefCode]"
+          >{{ prefecture.prefName }}</label
+        >
       </div>
     </div>
   </div>
@@ -89,8 +94,8 @@ usePopulationStore()
     <h2 class="subheader m-1"><span class="borderedText">人口のタイプ</span></h2>
     <div class="dataTypes flex flex-column-m flex-wrap w-full gap-3">
       <div class="w-1/3 sm:w-1/4 md:w-1/6 lg:w-1/6 xl:w-1/6 dataTypes__item flex-auto grow-0 flex flex-column-s flex-nowrap content-center mx-auto" v-for="label in labels" :key="label">
-        <input v-model="selectedLabel" type="radio" class="w-4 h-4 border-2 rounded-sm shrink-0 mt-1 mx-2 accent-blue-800 checked:border-0" :value="label" name="selectedLabel" />
-        <label for="selectedLabel" class="whitespace-nowrap">{{ label }}</label>
+        <input v-model="selectedLabel" type="radio" class="w-4 h-4 border-2 rounded-sm shrink-0 mt-1 mx-2 accent-blue-800 checked:border-0 cursor-pointer" :value="label" name="selectedLabel" />
+        <label for="selectedLabel" class="whitespace-nowrap cursor-pointer" @click="selectedLabel = label">{{ label }}</label>
       </div>
     </div>
   </div>
