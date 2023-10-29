0cb7b008ab905f97850745f65a5afda5bc982324
athary <athary80@gmail.com>
2023-10-29 20:57:53 +0900

    chore: storybookの基本設定

---
+++ .storybook/preview.ts
@@ -0,0 +1,23 @@
+import { type Preview, setup } from '@storybook/vue3'
+import { createPinia } from 'pinia';
+import { type App } from 'vue';
+
+const pinia = createPinia();
+
+setup((app: App) => {
+  app.use(pinia);
+});
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+}
+
+export default preview
